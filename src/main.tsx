c45691ac628a276108256fd70b030fd0d4958252
Haize <haizethomas@outlook.com>
2026-09-16 15:38:42 -1000

    HashRouter + GitHub Pages demo for iPad Safari

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import { StoreProvider } from "./store/StoreContext";
 import "./styles.css";
@@ -8,9 +8,9 @@ import "./styles.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <StoreProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </StoreProvider>
   </React.StrictMode>
 );
